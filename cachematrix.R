c60c29a1f71ec3c7e98380debb3236e3c08c1353
Rudy Neeser <rudy.neeser@gmail.com>
2014-09-21 11:46:05 +0200

    First pass at makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes and returns a list. This list 
+## provides access to the matrix, as well as to a cached
+## copy of the matrix's inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    
+    set <- function(m) {
+        x <<- m
+        i <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse=getinverse)
 }
 
 
